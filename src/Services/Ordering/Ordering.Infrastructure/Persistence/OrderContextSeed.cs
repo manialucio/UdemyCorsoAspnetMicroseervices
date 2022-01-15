eeac9f7dbc79822cb70e24057020a8151d7b94dc
manialucio <manialucio@gmail.com>
2022-01-15 23:10:18 +0100

    Services Ordering (no migrations)

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static List<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+            };
+    
+        }
+
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreConfiguredOrders());
+            }
+        }
+ 
+    }
+}
