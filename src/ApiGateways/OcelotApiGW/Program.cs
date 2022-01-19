4a54183e33301f5411913b143fa56ee414e497a0
manialucio <manialucio@gmail.com>
2022-01-19 23:28:53 +0100

    fatto ocelot iniziato aggregato

--- src/ApiGateways/OcelotApiGW/Program.cs
+++ src/ApiGateways/OcelotApiGW/Program.cs
@@ -1,9 +1,34 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.AddConfiguration()
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName }.json", true, true);
+builder.Services.AddOcelot().AddCacheManager(
+    s=>s.WithDictionaryHandle());
+
+ConfigureApplicationServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
+
+await app.UseOcelot();
+
 
 app.Run();
+
+void ConfigureApplicationServices(IServiceCollection services, ConfigurationManager configuration)
+{
+
+
+}
