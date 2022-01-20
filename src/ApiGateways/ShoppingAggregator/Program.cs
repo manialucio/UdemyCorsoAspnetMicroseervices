ddb8b8fabe31afd2e5fed0308f9c173fa31af32e
manialucio <manialucio@gmail.com>
2022-01-20 22:27:35 +0100

    fatto aggregator (da fare security?)

--- src/ApiGateways/ShoppingAggregator/Program.cs
+++ src/ApiGateways/ShoppingAggregator/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-ConfigureApplicationServices(builder.Services);
+ConfigureApplicationServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,12 +20,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
 
-static void ConfigureApplicationServices(IServiceCollection services)
+static void ConfigureApplicationServices(IServiceCollection services, IConfiguration configuration)
 {
+    services.AddHttpClient<ICatalogService, CatalogService>(c =>
+      c.BaseAddress = new Uri(configuration["ApiSettings:CatalogUrl"]));
+
+    services.AddHttpClient<IBasketService, BasketService>(c =>
+        c.BaseAddress = new Uri(configuration["ApiSettings:BasketUrl"]));
+
+    services.AddHttpClient<IOrderingService, OrderingService>(c =>
+        c.BaseAddress = new Uri(configuration["ApiSettings:OrderingUrl"]));
+    services.AddControllers();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shopping.Aggregator", Version = "v1" });
+    });
+
 };
